29d6e684bf3c6df15b5a2128d265998400a64f92
SwiftSki <49699645+SwiftSki@users.noreply.github.com>
2020-04-21 10:51:51 -0600

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -3,3 +3,12 @@ var jq = document.createElement('script');
 jq.src="https://code.jquery.com/jquery-3.5.0.js";
 document.body.appendChild(jq);
 alert('page successfully jQuerified!');
+
+var board = document.createElement('div');
+board.id="board";
+board.width="50%";
+board.height="50%";
+board.top="25%";
+board.left="25%";
+board.background="rgba(250,250,250,0.7)";
+document.body.appendChild(board);
